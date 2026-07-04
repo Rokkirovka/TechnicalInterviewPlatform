53a1a98a78cdd218cc44ff818b41f315617ffb32
NoteRokkirovka <rokkirovka64@yandex.ru>
2026-07-04 11:49:17 +0300

    added migration

--- src/Backend/Api/Program.cs
+++ src/Backend/Api/Program.cs
@@ -1,9 +1,14 @@
 using Api.Extensions;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.AddSerilogLogging();
 builder.AddExceptionHandler();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
